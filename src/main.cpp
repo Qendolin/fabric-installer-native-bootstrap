698006782fcd16779f718d2713f54c453af3e3db
Qendolin <32160662+Qendolin@users.noreply.github.com>
2026-05-22 16:18:48 +0200

    Refresh environment at start

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,61 @@
 #include "SystemHelper.h"
 #include "Logger.h"
 
-#include <stdexcept>
+#include <Userenv.h>
+#include <memory>
+#include <string_view>
+#include <string>
+
+#pragma comment(lib, "userenv.lib")
+
+namespace {
+	// RAII deleter for Win32 handles
+	struct HandleDeleter {
+		void operator()(HANDLE h) const {
+			if (h && h != INVALID_HANDLE_VALUE) {
+				::CloseHandle(h);
+			}
+		}
+	};
+
+	// RAII deleter for the Environment Block
+	struct EnvBlockDeleter {
+		void operator()(LPVOID p) const {
+			if (p) {
+				::DestroyEnvironmentBlock(p);
+			}
+		}
+	};
+
+	void RefreshEnvironment() {
+		HANDLE rawToken = NULL;
+		if (!::OpenProcessToken(::GetCurrentProcess(), TOKEN_QUERY | TOKEN_DUPLICATE, &rawToken)) {
+			return;
+		}
+		std::unique_ptr<void, HandleDeleter> hToken(rawToken);
+
+		LPVOID rawEnvBlock = nullptr;
+		// FALSE: build from system/user profile, not inheriting stale process env
+		if (!::CreateEnvironmentBlock(&rawEnvBlock, hToken.get(), FALSE)) {
+			return;
+		}
+		std::unique_ptr<void, EnvBlockDeleter> envBlock(rawEnvBlock);
+
+		const wchar_t* pVar = static_cast<const wchar_t*>(envBlock.get());
+		while (*pVar != L'\0') {
+			std::wstring_view entry(pVar);
+			const size_t eqPos = entry.find(L'=');
+
+			if (eqPos != std::wstring_view::npos && eqPos > 0) {
+				std::wstring name(entry.substr(0, eqPos));
+				std::wstring value(entry.substr(eqPos + 1));
+				::SetEnvironmentVariableW(name.c_str(), value.c_str());
+			}
+
+			pVar += entry.length() + 1;
+		}
+	}
+}
 
 _Use_decl_annotations_ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
 	UNREFERENCED_PARAMETER(hInstance);
@@ -27,6 +81,8 @@ _Use_decl_annotations_ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevI
 
 	// https://docs.microsoft.com/en-us/windows/win32/api/heapapi/nf-heapapi-heapsetinformation
 	::HeapSetInformation(::GetProcessHeap(), HeapEnableTerminationOnCorruption, NULL, 0);
+
+	RefreshEnvironment();
 
 	SystemHelper systemHelper;
 	Logger logger { systemHelper };
